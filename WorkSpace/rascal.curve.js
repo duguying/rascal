bbc153f6f517e8d392dbe8fee492386d32c60da3
duguying2008@gmail.com <duguying2008@gmail.com@e5aa44e6-cf2d-d60b-1980-10480643ba89>
2012-07-28 15:53:54 +0000

    git-svn-id: http://rascal-datashow.googlecode.com/svn/trunk@9 e5aa44e6-cf2d-d60b-1980-10480643ba89

--- WorkSpace/rascal.curve.js
+++ WorkSpace/rascal.curve.js
@@ -1,17 +1,20 @@
 // JavaScript Document
+// Created By RexLee
 inf=function(){
 	  inf.x=document.getElementById("box").offsetLeft;
 	  inf.y=document.getElementById("box").offsetTop;
 	  inf.w=document.getElementById("box").offsetWidth;
 	  inf.h=document.getElementById("box").offsetHeight;
-	  //alert(inf.w-2);
+	  inf.line_start=0;
+	  inf.line_l=0;
+	  inf.line_start_x;
+	  inf.line_end_x;
 	};
 	
 (function(){
 	setting={};
 	setting.arrname="arr";
 	setting.point_color="red";
-	
 	})();
 
 curve=function(){
@@ -24,15 +27,24 @@ curve=function(){
 	  document.getElementById("box").appendChild(c_point);
 	}
 	
-	curve.move=function(p,x,y){
-	  m_point=document.getElementById("p"+p);
+	curve.move=function(p,x,y,l){//l为点型，即为点的长度
+	  m_point=document.getElementById("p"+Math.floor(p));//字母f不能传进来
 	  m_point.style.borderStyle="solid";
 	  m_point.style.borderColor=setting.point_color;
 	  m_point.style.borderWidth="1px";
+	  try{
+		  if(!(x==inf.line_start_x)){
+			  m_point.style.height=inf.line_l+"px";
+			  };
+		  if(x==inf.line_end_x){
+				m_point.style.height=inf.line_l*0+"px";
+			  };
+		  }catch(e){};
 	  m_point.style.left=x+"px";
 	  m_point.style.top=y+"px";
+	  
 	}
-	
+
 	curve.init=function(){
 		inf();
 		for(i=0;i<inf.w-3;i++){
@@ -47,31 +59,30 @@ curve=function(){
 		var dx=x2-x1;
 		var dy=y2-y1;
 		var ddy=dy/dx;
-		//alert(ddy);
-		for(var i=0;i<dx;i++){
-			curve.move(i,x1+i,y1+i*ddy-fix_y);
-			}
+		x1=Math.floor(x1);
+		x2=Math.floor(x2);//次出将x取整，不然40行无法抓取线段末尾点
+		inf.line_l=Math.abs(ddy);
+		inf.line_start_x=x1;
+		inf.line_end_x=x2;
+		for(var i=0;i<=dx;i++){
+			try{curve.move(inf.line_start+i,x1+i,y1+i*ddy-fix_y);}catch(e){/*inf.line_end_x=i;alert(e+"  "+i);*/};//会溢出报错
+			};
 		}
-	
-	
+
 	curve.draw=function(){
 		var arr=eval(setting.arrname);
 		var len=arr.length;
-		//alert(len);
 		var d=inf.w/(len-1);
 		for(var i=0;i<len;i++){
 			curve.line(i*d,arr[i],(i+1)*d,arr[i+1]);
-			alert("pause");
+			inf.line_start=(i+1)*d;
 			if(i==len-2){break;};
 			}
 
-		}	
-	
-	
-	
+		};
+		
+
+	//启动画图
 	curve.init();
 	curve.draw();
-	
-
-
 };
